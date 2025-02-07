73b54e2c0db25774579442b24f69366a26e238b6
0xnuru <0xnuru@gmail.com>
2025-02-07 17:23:02 +0100

    feat: add app initial commit

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+from app.routers import mail
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5173",
+        "http://localhost:5173",
+        "https://www.madeforemore.ca",
+        "https://madeforemore.ca",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(mail.router)
